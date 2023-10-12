6c729b3f23ebb21bb426d980d90f29bc0ad784f9
JamilyaHalyyeva <jamilya.halyyeva@gmail.com>
2023-10-12 14:01:17 +0200

    adds: filtering with another property

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const listBody = document.querySelector("#list-body");
 let storedBreeds=[];
 let filteredBreeds=[];
+let breedGroup=[];
 document.querySelector("#btnSearch").addEventListener("click",filterBreeds);
 function createRow(name, imageURL, description) {
   return `<div class="media flex-column flex-sm-row mt-0 mb-3">
@@ -38,12 +39,14 @@ async function populate(){
         storedBreeds.forEach(dog => {
             let imgUrl =`https://cdn2.thedogapi.com/images/${dog.reference_image_id}.jpg`
             listBody.innerHTML += createRow(dog.name,imgUrl,dog.temperament);
-        });  
+        }); 
+        breedGroup = [...new Set(storedBreeds.map(breed=>breed.breed_group))];
+        console.log("breed Group:",breedGroup )
     } catch (error) {
         
+    }   finally{
+        populateBreedGroupSelect()
     }
-   
-    
     // listBody.innerHTML += createRow("baggy","jgyhbikjn","kjdsfjfbhjdbfhjsdbhjb");
 }
 
@@ -54,15 +57,42 @@ async function getListOfDogs(breed){
 
 populate();
 
-
+function populateBreedGroupSelect(){
+    let breedGroupSelect= document.querySelector("#select_breed_group")
+    breedGroup.forEach(bg=>{
+        var option = document.createElement("option");
+        option.text = bg; 
+        option.value= bg;
+        breedGroupSelect.add(option);
+    })
+}
 
 
 function filterBreeds(){
 
-    let filter_name=document.querySelector("#searchbox_name");
-
+    let filter_name=document.querySelector("#searchbox_name").value;
+    let filter_breed_group = document.querySelector("#select_breed_group").value;
+    console.log(filter_breed_group)
     filteredBreeds = storedBreeds.filter((breed)=>{
-       return breed.name.includes(filter_name.value);
+        let result_name =false;
+        let result_breed_group=false;
+        if( filter_name &&  filter_name !=="" && filter_name.length>0){
+            result_name = breed.name.includes(filter_name)
+        }
+        if( filter_breed_group !== 0 ){
+            result_breed_group = breed.breed_group===filter_breed_group
+        }
+        if(filter_name ==="" &&  filter_breed_group === '0'){
+            return true;
+        }else if(filter_name ===""&&  filter_breed_group !== '0'){
+            return result_breed_group;
+        }else if(filter_name !==""&&  filter_breed_group === '0'){
+            return result_name;
+        }
+        else{
+            return result_name && result_breed_group;
+        }
+       
     })
     console.log(filteredBreeds)
     console.log(filter_name.value);
